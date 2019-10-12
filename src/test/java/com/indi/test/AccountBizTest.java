86650fc6fdfb3f9bb73b7d435190ada55afb4ca7
feng345fengcool <feng345fengcool@163.com>
2019-10-12 19:36:22 +0800

    rename

--- src/test/java/com/indi/test/AccountBizTest.java
+++ src/test/java/com/indi/test/AccountBizTest.java
@@ -10,7 +10,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = {SpringConfig.class})
-public class AccountServiceTest {
+public class AccountBizTest {
 
     @Autowired
     private AccountBiz as;
